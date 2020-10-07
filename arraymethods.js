eb73bf64236a7487a4c9ef7e89f4625304f6b182
Nova Eeken <novaeeken@novas-mbp.home>
2020-10-07 13:42:31 +0200

    Edit uitwerkingen

--- arraymethods.js
+++ arraymethods.js
@@ -5,12 +5,42 @@ const emailadresses = ['n.eeken@novi.nl', 'n.stuivenberg@novi.nl', 'm.vandergees
 
 // 1a. Onze nieuwe medewerker Melissa moet worden toegevoegd aan de lijst met emailadressen. Haar email is: m.westerbroek@novi.nl
 
+emailadresses.push('m.westerbroek@novi.nl');
+console.log(emailadresses);
 
 // 1b. Jouw collega wil weten of het emailadres van Nick Stuivenberg in de lijst staat. Zoek dit voor hem uit. (dus: bevat onze array n.stuivenberg@novi.nl?)
+
+// OPTIE 1
+const isNickThere = emailadresses.includes('n.stuivenberg@novi.nl');
+console.log(isNickThere);
+
+
+// OPTIE 2
+// if (isNickThere === true) {
+//     console.log("Nick is er!");
+// }
+
+// OPTIE 3
+// if (emailadresses.includes('n.stuivenberg@novi.nl')) {
+//     console.log("Nick is er!");
+// }
+
+// OPTIE 4
+// console.log(emailadresses.includes('n.stuivenberg@novi.nl') && 'Nick bestaat!');
+
 
 
 // 1c. Collega's staan in de emaillijst op volgorde waarin ze bij Novi zjin komen werken. Nu blijkt echter dat een andere collega, Tess,
 // bij Novi is komen werken VOOR Melissa. Haar e-mailadres moet dus worden toegevoegd op de één na laatste plek: t.mellink@novi.nl
+
+// OPTIE 1
+const insertPlace = emailadresses.length - 1;
+emailadresses.splice(insertPlace, 0, 't.mellink@novi.nl');
+console.log(emailadresses);
+
+// OPTIE 2: als we  niet zouden weten waar melissa staat in de array
+// emailadresses.splice(emailadresses.indexOf("m.westerbroek@novi.nl"), 0, "t.mellink@novi.nl");
+
 
 
 // 1c. Er staat een foutje in de lijst. Het email adres van Mitchel moet worden aangepast naar m.vandergeest@novi.nl, maar je weet niet
@@ -18,3 +48,12 @@ const emailadresses = ['n.eeken@novi.nl', 'n.stuivenberg@novi.nl', 'm.vandergees
 // een array met emailadressen, het oude emailadres dat vervangen moet worden, en het nieuwe emailadres.
 // Deze functie achterhaalt het indexnummer van het oude emailadres en vervangt dit vervolgens voor het nieuwe adres.
 // TIP: schrijf 'm eerst zoals je hem zou toepassen op bovenstaande array en schrijf die code dan om naar een functie
+
+function replaceEmail(emailList, wrongEmail, replacementEmail) {
+    const index = emailList.indexOf(wrongEmail);
+    // ons geval staat hier eigenlijk, emailadresses[2] = m.vandergeest@novi.nl
+    emailList[index] = replacementEmail;
+}
+
+replaceEmail(emailadresses, 'm.vandergeest@novi-education.nl', 'm.vandergeest@novi.nl');
+console.log(emailadresses);
--- otherMethods.js
+++ otherMethods.js
@@ -6,7 +6,11 @@
 // 4 geeft 12.566370614359172
 // 12 geeft 37.69911184307752
 
+function perimeter(diameter) {
+    return Math.PI * diameter;
+}
 
+console.log(perimeter(4));
 
 /* Opdracht 2 */
 // Schrijf bovenstaande functie nogmaals, maar zorg er nu voor dat de uitkomst wordt afgerond
@@ -15,6 +19,11 @@
 // 12 geeft 38
 
 
+function perimeterRounded(diameter) {
+    return Math.round(Math.PI * diameter);
+}
+
+perimeterRounded(4);
 
 /* Opdracht 3 */
 // Schrijf een functie die het laatste karakter in een string teruggeeft
@@ -22,7 +31,18 @@
 // "vermicelli" geeft "i"
 // "kroepoek" geeft "k"
 
+// OPTIE 1 (geeft het karakter op dit indexnummer terug)
+function lastCharacter(word) {
+    const index = word.length - 1;
+    // const index = word.lastIndexOf(word);
+    return word.charAt(index);
+}
 
+// OPTIE 2 (knipt de laatste letter van het woord eraf)
+const lastChar = word => word.slice(-1);
+
+console.log(lastCharacter("vermicelli"));
+console.log(lastChar("vermicelli"));
 
 
 /* Opdracht 4 */
@@ -32,15 +52,21 @@
 // " De pot verwijt de ketel dat hij zwart ziet" geeft ['De', 'pot', 'verwijt', 'de', 'ketel', 'dat', 'hij', 'zwart', 'ziet'];
 // "Niet geschoten is altijd mis " geeft [ 'Niet', 'geschoten', 'is', 'altijd', 'mis' ];
 
+function split(saying) {
+    return saying.trim().split(" ");
+}
 
-
+console.log(split("Niet geschoten is altijd mis "));
 
 /* Opdracht 5 */
 // Maak een variabele today aan en sla daar een nieuw date-object in op
 // Print in de console (in nummers) in welke maand we nu zitten
 // Print in de console (in nummers) welk uur van de dag het is
 
+const today = new Date();
 
+console.log(today.getMonth());
+console.log(today.getHours());
 
 /* BONUS OPDRACHT (als je tijd over hebt) */
 // Schrijf een functie die twee strings als parameters verwacht en de langste naam teruggeeft.
@@ -49,5 +75,23 @@
 // "Nick", "Nova" geeft "Beide namen zijn even lang"
 // "Mitchel", "Nick" geeft "Mitchel"
 
+// OPTIE 1
+function longestName(nameOne, nameTwo) {
+    const firstName = nameOne.length;
+    const secondName = nameTwo.length;
 
+    if (firstName === secondName) {
+        return "Beide namen zijn even lang";
+    }
+    // Mag ook als:
+    // return nameOne.length === nameTwo.length && "Beide namen zijn even lang";
 
+    if (firstName > secondName) {
+        return nameOne;
+    }
+
+    return nameTwo;
+}
+
+console.log(longestName("Mitchel", "Nick"));
+console.log(longestName("Nova", "Nick"));
